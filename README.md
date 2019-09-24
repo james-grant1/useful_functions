7f651ad0d9aa816f1a23ff85bae5321cd38e2344
James Grant <rjg20@bath.ac.uk>
2019-09-24 14:27:03 +0000

    Added check for empty list and readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Some useful functions
+
+Contains:
+
+A mean function to return the mean of a list, checks not an empty list first
--- functions.py
+++ functions.py
@@ -2,6 +2,10 @@ def mean(this_list):
     '''
     Takes a list of number and returns the mean
     '''
+    
+    if len(this_list) <= 0:
+        return "You passed an empty list"
+    
     total=0
     for item in list:
         total = total + list
