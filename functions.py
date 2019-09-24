e2455be3b1521cac81e853a212a31c22f0d328c1
James Grant <rjg20@bath.ac.uk>
2019-09-24 14:52:15 +0000

    Randomly deleted lines of text

--- functions.py
+++ functions.py
@@ -3,11 +3,6 @@ def mean(this_list):
     Takes a list of number and returns the mean
     '''
     
-    if len(this_list) <= 0:
-        return "You passed an empty list"
-    
-    list_example = [1.0,2.0]
-    
     if type(this_list) != type(list_example):
         return "You didn't pass a list"
     
